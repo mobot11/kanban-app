b8be5c90ceee617d1117dacc63f7eb738efaea52
Dylan T. Moberg <dylanmoberg@msn.com>
2016-08-19 23:11:08 -0700

    adding comments

--- app/components/Notes.jsx
+++ app/components/Notes.jsx
@@ -4,19 +4,17 @@ import Note from './Note';
 import Editable from './Editable';
 
 
-export default ({
+//so heavy on dat es6 lol
+export default({
     notes,
-    onNoteClick=() => {}, onEdit=() => {}, onDelete=() => {}
+    onNoteClick = () => {},
+    onEdit = () => {},
+    onDelete = () => {}
 }) => (
-    <ul className="notes">{notes.map(({id, editing, task}) =>
-  <li key={id}>
-    <Note className="note" onClick={onNoteClick.bind(null, id)}>
-      <Editable className="editable"
-         editing={editing}
-         value={task}
-         onEdit={onEdit.bind(null, id)} />
-       <button className="delete" onClick={onDelete.bind(null, id)}>xx</button>
-    </Note>
-  </li>
-)}</ul>
+    <ul className="notes">{notes.map(({id, editing, task}) => <li key={id}>
+            <Note className="note" onClick={onNoteClick.bind(null, id)}>
+                <Editable className="editable" editing={editing} value={task} onEdit={onEdit.bind(null, id)}/>
+                <button className="delete" onClick={onDelete.bind(null, id)}>xx</button>
+            </Note>
+        </li>)}</ul>
 )
